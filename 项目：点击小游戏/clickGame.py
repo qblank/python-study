e647a31481979808acd2142cbb97ca59f9331c0c
evan_qb <798046397@qq.com>
2020-05-19 14:24:01 +0800

    项目：点击小游戏

---
+++ 项目：点击小游戏/clickGame.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/19 13:24
+# @Author  : evan_qb
+# @Site    : 
+# @File    : clickGame.py
+# @Software: PyCharm
+
+from selenium import webdriver
+import time
+
+brower = webdriver.Chrome()
+brower.get("http://www.sorpack.com/dv_plus/game/csgaoshou.htm")
+try:
+    startStopBtn = brower.find_element_by_name("startstop")
+    startStopBtn.click()
+
+    radioBoxs = brower.find_elements_by_xpath("//input[@type='radio']")
+    while True:
+        for radioBox in radioBoxs:
+            if radioBox.is_selected():
+                radioBox.click()
+        timeValue = brower.find_element_by_name("timeleft").get_attribute("value")
+        print(timeValue)
+        if int(timeValue) == 1:
+            print("exit")
+            break
+
+finally:
+    print("结束")
+
